be9bbe104ac37171f2ff83b758e655cd8b5bdcd4
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2022-11-29 14:47:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -172,8 +172,8 @@ async def cbconfess(client, query: CallbackQuery):
 
   
 
-        confesss =
-await client.send_message(DEV, f"from {query.from_user.mention}\nisi : {confesss.text}")
+        confesss = isi
+report = await client.send_message(DEV, f"from {query.from_user.mention}\nisi : {confesss.text}")
 
   confesss = isi
     report = await client.send_message(LOG, f"<b>From :</b> <i>{nama}</i>\n<b>To :</b> <i>{to.text}</i>\n<b>Isi :</b> <i>{confesss.text}</i>", disable_web_page_preview=True)
