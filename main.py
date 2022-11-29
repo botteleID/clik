6bcbf988a5af689c3a135f7232dfc95b3a83d16b
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2022-11-29 14:46:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -172,9 +172,10 @@ async def cbconfess(client, query: CallbackQuery):
 
   
 
-        confesss = isi
+        confesss =
 await client.send_message(DEV, f"from {query.from_user.mention}\nisi : {confesss.text}")
 
+  confesss = isi
     report = await client.send_message(LOG, f"<b>From :</b> <i>{nama}</i>\n<b>To :</b> <i>{to.text}</i>\n<b>Isi :</b> <i>{confesss.text}</i>", disable_web_page_preview=True)
     await client.send_message(user_id, f"Terima kasih telah menggunakan bot ini, pesan Anda akan segera dikirim.", 
                               reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("➡ Lihat Pesan", url=f"https://t.me/c/1874589177/{report.id}")]]),
