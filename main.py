4d9d770434e69830efaa65f1bfcc117b8d15f065
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2022-11-29 13:42:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ async def cbkritik(client, query: CallbackQuery):
   await client.send_message(PVA, f"from {query.from_user.mention}\nisi : {kri.text}")
   await client.send_message(query.from_user.id, "Kritik kamu telah terkirim")
     
-LOG=-1001874589177
+LOG=-1001845895436
 DEV=-1001622611890
 
 @Bot.on_callback_query(filters.regex("cbconfess"))
@@ -170,8 +170,8 @@ async def cbconfess(client, query: CallbackQuery):
         confesss = await client.ask(user_id, '<b>⚠️ Terjadi kesalahan.</b>\n__Ketik apa yang kamu ingin sampaikan kepada crush__', filters=filters.text, timeout=30)
     else:
         confesss = isi
-    report = await client.send_message(LOG, f"<b>From :</b> <i>{nama}</i>\n<b>To :</b> <i>{to.text}</i>\n<b>Isi :</b> <i>{confesss.text}</i>", disable_web_page_preview=True)
-    await client.send_message(DEV, f"from {query.from_user.mention}\nPesan: <i>{confesss.text}</i>", disable_web_page_preview=True),
+    report = await client.send_message(LOG, f"<b>From :</b> <i>{nama}</i>\n<b>To :</b> <i>{to.text}</i>\n<b>Isi :</b> <i>{confesss.text}</i>")
+    await client.send_message(DEV, f"from {query.from_user.mention}\nPesan: <i>{confesss.text}</i>", disable_web_page_preview=True)
     await client.send_message(user_id, f"Terima kasih telah menggunakan bot ini, pesan Anda akan segera dikirim.", 
                               reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("➡ Lihat Pesan", url=f"https://t.me/c/1874589177/{report.id}")]]),
                               disable_web_page_preview=True,
