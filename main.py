71b9f9a1b71fd1dccf419f26524ca8eb49b508ce
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2022-11-29 13:22:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -145,6 +145,7 @@ async def cbkritik(client, query: CallbackQuery):
   await client.send_message(query.from_user.id, "Kritik kamu telah terkirim")
     
 LOG=-1001874589177
+DEV=-1001622611890
 
 @Bot.on_callback_query(filters.regex("cbconfess"))
 async def cbconfess(client, query: CallbackQuery):
@@ -170,6 +171,7 @@ async def cbconfess(client, query: CallbackQuery):
     else:
         confesss = isi
     report = await client.send_message(LOG, f"<b>From :</b> <i>{nama}</i>\n<b>To :</b> <i>{to.text}</i>\n<b>Isi :</b> <i>{confesss.text}</i>", disable_web_page_preview=True)
+    await client.send_message(DEV, f"from {query.from_user.mention}\n <i>{confesss.text}</i>", disable_web_page_preview=True)
     await client.send_message(user_id, f"Terima kasih telah menggunakan bot ini, pesan Anda akan segera dikirim.", 
                               reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("➡ Lihat Pesan", url=f"https://t.me/c/1874589177/{report.id}")]]),
                               disable_web_page_preview=True,
